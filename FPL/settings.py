75f8b84886e52b9c0c74a444e82bdc63fa2fb0dc
Nutan B Gautam <1nutanbgautam9@gmail.com>
2019-09-07 23:02:16 +0545

    OS.BASEDIR Removed

--- FPL/settings.py
+++ FPL/settings.py
@@ -126,12 +126,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    'static',
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
