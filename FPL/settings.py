da3d9e3738ca7165c49ec25860e49d22bdbe50e6
Nutan B Gautam <1nutanbgautam9@gmail.com>
2019-09-07 20:59:16 +0545

    space removed in profile

--- FPL/settings.py
+++ FPL/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"Static-Files-FPL")
 django_heroku.settings(locals())
