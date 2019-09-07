1b30e4e4bae706b307a3ab6a556324276182212b
Nutan B Gautam <1nutanbgautam9@gmail.com>
2019-09-07 21:15:08 +0545

    space removed in profile

--- FPL/settings.py
+++ FPL/settings.py
@@ -119,6 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static.'
-STATIC_ROOT=os.path.join("Static-Files-FPL")
+# STATIC_URL = '/static/'
+# STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"Static-Files-FPL")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
