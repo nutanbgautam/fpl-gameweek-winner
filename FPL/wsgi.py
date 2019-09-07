6a642483a36a933688fb19e87081bbae615600cf
Nutan B Gautam <1nutanbgautam9@gmail.com>
2019-09-07 18:49:34 +0545

    Second Commit

--- FPL/wsgi.py
+++ FPL/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FPL.settings')
+
+# application = get_wsgi_application()
+
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FPL.settings")
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FPL.settings')
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
