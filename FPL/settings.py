fa094cb2fc45bb0b1dcba311bed50ee4fdfa656c
Nutan B Gautam <1nutanbgautam9@gmail.com>
2019-09-07 18:39:03 +0545

    Second Commit

--- FPL/settings.py
+++ FPL/settings.py
@@ -121,10 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static-storage")
-]
-
-STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static-serve")
-
 django_heroku.settings(locals())
